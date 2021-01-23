d9374ee30046fdb0ee46ff2edef7a0c30a8631de
Yankee-Code-Glitch <77178415+Yankee-Code-Glitch@users.noreply.github.com>
2021-01-22 16:18:15 -0800

    Move update
    You can now move up and down.

--- Pyng.py
+++ Pyng.py
@@ -1,8 +1,19 @@
 from turtle import *
+def moveup():
+    player.left(90)
+    player.forward(15)
+    player.right(90)
+def movedown():
+    player.right(90)
+    player.forward(15)
+    player.left(90)
+
+
 Window = Screen()
 Window = title("Pyng")
-Window = bgcolor("#00ff43")
+Window = bgcolor("#0b005e")
 Window = setup(1000,450)
+
 topBar = Turtle()
 topBar.shape("square")
 topBar.pensize(15)
@@ -12,6 +23,7 @@ topBar.hideturtle()
 topBar.goto(460,190)
 topBar.down()
 topBar.goto(-465,190)
+
 lowerBar = Turtle()
 lowerBar.shape("square")
 lowerBar.pensize(15)
@@ -21,4 +33,17 @@ lowerBar.hideturtle()
 lowerBar.goto(-465,-190)
 lowerBar.down()
 lowerBar.goto(465,-190)
+
+player = Turtle()
+player.shapesize(3)
+player.color("#d6d6d6")
+player.penup()
+player.hideturtle()
+player.goto(-450,0)
+player.showturtle()
+player.speed(9999)
+
+onkeypress(moveup,"Up")
+onkeypress(movedown,"Down")
+listen()
 mainloop()
